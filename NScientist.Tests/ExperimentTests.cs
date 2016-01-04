05813a30e5fe727b99daff2b71af66ae7208461a
andy <andy@andydote.co.uk>
2016-01-04 20:12:32 +0000

    experiment: add basic fluentinterface and tests

---
+++ NScientist.Tests/ExperimentTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace NScientist.Tests
+{
+	public class ExperimentTests
+	{
+		[Fact]
+		public void When_running_a_void_action()
+		{
+			var control = false;
+			var test = false;
+
+			Experiment
+				.On(new Action(() => control = true))
+				.Try(() => test = true)
+				.Run();
+
+			control.ShouldBe(true);
+			test.ShouldBe(true);
+		}
+
+		[Fact]
+		public void When_running_an_action_with_result()
+		{
+			var control = 0;
+			var test = 0;
+
+			var result = Experiment
+				.On(() => control += 10)
+				.Try(() => test += 20)
+				.Run();
+
+			control.ShouldBe(10);
+			test.ShouldBe(20);
+			result.ShouldBe(10);
+		}
+	}
+}
---
+++ NScientist/Experiment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NScientist
+{
+	public class Experiment
+	{
+		public static ExperimentConfig<object> On(Action action)
+		{
+			return new ExperimentConfig<object>();
+		}
+
+		public static ExperimentConfig<T> On<T>(Func<T> action)
+		{
+			return new ExperimentConfig<T>();
+		}
+	}
+
+	public class ExperimentConfig<TResult>
+	{
+		public ExperimentConfig<TResult> Try(Action action)
+		{
+			throw new NotImplementedException();
+		}
+
+		public TResult Run()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
