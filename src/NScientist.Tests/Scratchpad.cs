134094376bf70526bfb27b35e95c35cb92f7ca5f
andy <andy@andydote.co.uk>
2016-08-02 19:08:29 +0100

    tests: add fake test to check dotnet test works

---
+++ src/NScientist.Tests/Scratchpad.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace NScientist.Tests
+{
+	public class Scratchpad
+	{
+		[Fact]
+		public void When_testing_something()
+		{
+			
+		}
+
+		[Fact]
+		public void When_passing()
+		{
+			Assert.Equal(4, Add(2, 2));
+		}
+
+		[Fact]
+		public void When_failing()
+		{
+			Assert.Equal(5, Add(2, 2));
+		}
+
+		private static int Add(int x, int y) => x + y;
+	}
+}
